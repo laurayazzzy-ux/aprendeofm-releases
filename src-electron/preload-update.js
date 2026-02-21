700646df00c8d0ef0cd8b8bb85d5b18cb96a0b43
Pollastra <laura.yazzzy@gmail.com>
2026-02-21 06:12:39 +0400

    Clean repo - releases only

--- src-electron/preload-update.js
+++
@@ -1,5 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('updateAPI', {
-  onStatus: (callback) => ipcRenderer.on('update:status', (event, data) => callback(data))
-});
